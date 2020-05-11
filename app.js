0f5543c0b2e2f893f51e41452bf22e46af536940
Marc Barker <marcbarker6@gmail.com>
2020-05-11 17:23:22 +0100

    Create basic app with index page.

---
+++ app.js
@@ -0,0 +1,19 @@
+// majority courtesy of freecodecamp at https://github.com/freeCodeCamp/boilerplate-project-timestamp/blob/gomix/server.js
+
+const express = require('express');
+const app = express();
+
+const cors = require('cors')
+app.use(cors({optionsSuccessStatus: 200}))
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'))
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/views/index.html')
+})
+
+const listener = app.listen(process.env.PORT, function () {
+    console.log('Your app is listening on port ' + listener.address().port);
+});
