d15c09c030720fd48953a9d730987e7ec697d015
Marc Barker <marcbarker6@gmail.com>
2020-05-16 09:46:26 +0100

    Setup Heroku deployment.

--- src/app.js
+++ src/app.js
@@ -14,6 +14,6 @@ app.get('/', function(req, res) {
     res.sendFile(__dirname + '/views/index.html')
 })
 
-const listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(process.env.PORT || 3000, function () {
     console.log('Your app is listening on port ' + listener.address().port);
 });
